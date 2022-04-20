417aa2c6c38a3facd27522aa8a43ec8a708a3258
Martin Dobrev <martindobrev87@gmail.com>
2022-04-20 15:23:42 +0300

    HTS operations added

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const {
   AccountId,
   PublicKey,
   PrivateKey,
+  TokenId,
   Hbar,
   AccountCreateTransaction,
+  TokenCreateTransaction,
+  TokenAssociateTransaction,
+  TokenDissociateTransaction,
+  TransferTransaction,
+  AccountBalanceQuery,
+  TokenDeleteTransaction,
 } = require('@hashgraph/sdk');
 const { hethers } = require('@hashgraph/hethers');
 require('dotenv').config();
@@ -13,12 +20,11 @@ const contractJSON = require('./contracts/abi/combined.json');
 
 console.clear();
 
-const operatorId = AccountId.fromString(process.env.ACCOUNT_ID);
-const operatorKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
+const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
 
-if (operatorId == null || operatorKey == null) {
-  throw new Error('Environment variables ACCOUNT_ID and PRIVATE_KEY must be present');
-}
+const accountId = AccountId.fromString(process.env.ACCOUNT_ID);
+const accountKey = PrivateKey.fromString(process.env.ACCOUNT_KEY);
 
 // Init client
 const client = Client.forTestnet().setOperator(operatorId, operatorKey);
@@ -69,12 +75,101 @@ function getWallet(provider) {
   return wallet;
 }
 
+async function createToken(client, name, symbol, decimals, initialSupply, accountId) {
+  const createTokenTx = await new TokenCreateTransaction()
+    .setTokenName(name)
+    .setTokenSymbol(symbol)
+    .setDecimals(decimals)
+    .setInitialSupply(initialSupply)
+    .setTreasuryAccountId(accountId)
+    .execute(client);
+
+  const { tokenId } = await createTokenTx.getReceipt(client);
+
+  return tokenId;
+}
+
+async function associateToken(client, accountId, keyId, tokenId) {
+  const associateTokenTx = await new TokenAssociateTransaction()
+    .setAccountId(accountId)
+    .setTokenIds([tokenId])
+    .freezeWith(client)
+    .sign(keyId);
+
+  const submitTx = await associateTokenTx.execute(client);
+  const associateReceipt = await submitTx.getReceipt(client);
+
+  console.log('associateReceipt', associateReceipt);
+}
+
+async function dissociateToken(client, accountId, keyId, tokenId) {
+  const dissociateTokenTx = await new TokenDissociateTransaction()
+    .setAccountId(accountId)
+    .setTokenIds([tokenId])
+    .freezeWith(client)
+    .sign(keyId);
+
+  const submitTx = await dissociateTokenTx.execute(client);
+  const dissociateReceipt = await submitTx.getReceipt(client);
+
+  console.log('dissociateReceipt', dissociateReceipt);
+}
+
+async function deleteToken(client, keyId, tokenId) {
+  const deleteTokenTx = await new TokenDeleteTransaction()
+    .setTokenId(tokenId)
+    .freezeWith(client)
+    .sign(keyId);
+
+  const submitTx = await deleteTokenTx.execute(client);
+  const deleteReceipt = await submitTx.getReceipt(client);
+
+  console.log('deleteReceipt', deleteReceipt);
+}
+
+async function transferToken(client, fromId, toId, tokenId) {
+  const transferTx = await new TransferTransaction()
+    .addTokenTransfer(tokenId, fromId, -10)
+    .addTokenTransfer(tokenId, toId, 10)
+    .execute(client);
+
+  const transferReceipt = await transferTx.getReceipt(client);
+
+  console.log('transferReceipt', transferReceipt);
+}
+
+async function checkBalance(client, accountId) {
+  const checkBalanceTx = await new AccountBalanceQuery().setAccountId(accountId).execute(client);
+
+  console.log('checkBalanceTx', checkBalanceTx.tokens.toString());
+}
+
 async function main() {
   // Get provider
   const defaultProvider = hethers.providers.getDefaultProvider('testnet');
 
   // Get operator balance
   const operatorBalance = await defaultProvider.getBalance(operatorId);
+
+  // Create Token with HTS
+  // const tokenId = await createToken(client, 'Test 12345', 'TEST12345', 0, 1000, operatorId);
+
+  // const currentTokenId = new TokenId(0, 0, 34249493);
+
+  // Associate token to id
+  // await associateToken(client, accountId, accountKey, tokenId);
+
+  // Dissociate token to id
+  // await dissociateToken(client, accountId, accountKey, currentTokenId);
+
+  // Delete token
+  // await deleteToken(client, accountKey, currentTokenId);
+
+  // Transfer token
+  // await transferToken(client, accountId, operatorId, currentTokenId);
+
+  // Check account balance
+  await checkBalance(client, accountId);
 
   // Create Hethers wallet
   /*
@@ -84,7 +179,9 @@ async function main() {
   */
 
   // Init Hethers wallet
+  /*
   const wallet = getWallet(defaultProvider);
+  */
 
   // Deploy contract
   /*
@@ -93,13 +190,13 @@ async function main() {
   */
 
   // Get contract instance
+  /*
   const contractInstance = new hethers.Contract(
     process.env.CONTRACT_ADDRESS,
     contractJSON.contracts['contracts/Todo.sol:Todo'].abi,
     wallet,
   );
-
-  console.log('contractInstance', contractInstance);
+  */
 }
 
 main();
